f5adb500fe9316587c692dfac0e37cb53cf13539
mabsothea2022 <mabsothea9999@gmail.com>
2024-06-22 22:05:10 +0700

    Reactjs hook.

---
+++ src/components/organisms/layout/Layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MasterLayout:React.FC = () =>{
+    return(
+        <div>
+            <h1>Master Layout</h1>
+        </div>
+    );
+}
+
+export default MasterLayout;
